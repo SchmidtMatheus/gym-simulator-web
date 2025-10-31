88d1d6e78523c2f2955ee9eeeed3debf5b15cbc9
MatheusSchmidt <matheus.schmidt@outlook.com>
2025-10-30 21:35:42 -0300

    Feat: moved getPlanTypeColor into utils folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getPlanTypeColor = (planType: string) => {
+  switch (planType) {
+    case "Plano Anual":
+      return "bg-green-100 text-green-800";
+    case "Plano Trimestral":
+      return "bg-blue-100 text-blue-800";
+    case "Plano Mensal":
+      return "bg-yellow-100 text-yellow-800";
+    default:
+      return "bg-gray-100 text-gray-800";
+  }
+};
